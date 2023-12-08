5ebda1d8573c2f4f2518f733b70396e5a189152f
cordonnierc <corentincordonnier@hotmail.fr>
2023-12-08 12:36:38 +0100

    amélioration du code

--- 1/index.js
+++ 1/index.js
@@ -1,4 +1,6 @@
+const { log } = require('console');
 const fs = require('fs');
+const { exit } = require('process');
 const readline = require('readline');
 
 void (async () => {
@@ -9,8 +11,11 @@ void (async () => {
     
     var total = 0;
     rl.on('line', (line) => {
+        /*old version*/
+        /*
         var firstNumber = null;
         var lastNumber = null;
+        
         for(var i=0; i< line.length;i++){
             if(!isNaN(line.charAt(i))){
                 if(firstNumber == null){
@@ -18,8 +23,13 @@ void (async () => {
                 }
                 lastNumber = line.charAt(i);
             }
-        }
-        total += parseInt(firstNumber+lastNumber);
+            total += parseInt(firstNumber+lastNumber);
+        }*/
+        
+        
+        /*new version*/
+        let arrayNumbers = line.split("").map(value => parseInt(value,10)).filter(a=>a);
+        total += parseInt(""+arrayNumbers[0]+arrayNumbers[arrayNumbers.length-1]);
     });
     console.log(total);
     await new Promise((res) => rl.once('close', res));
